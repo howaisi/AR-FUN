825f0cdcc9c3d6273ee5001c9c3ba1e279dde9df
Hozaifa Owaisi <hozaifaowaisi.o@gmail.com>
2018-01-16 20:41:43 -0500

    Initial Commit

---
+++ ArFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArFun
+//
+//  Created by Hozaifa on 1/16/18.
+//  Copyright © 2018 Hozaifa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
